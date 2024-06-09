9cdd3e56179811a90f6a8391dd8361edd09c8168
kobakboa5884 <kobanminmin58@gmail.com>
2024-06-09 16:16:07 +0900

    add template and mybatis dependencis

--- mybatis-crud-api/build.gradle.kts
+++ mybatis-crud-api/build.gradle.kts
@@ -20,18 +20,18 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.0.3")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
+	implementation("org.jetbrains.kotlin:kotlin-reflect:2.0.0")
 	// implementation("org.mybatis:mybatis:3.5.6")
-	// implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.2.1")
+	implementation("org.mybatis.dynamic-sql:mybatis-dynamic-sql:1.5.2")
 	implementation("org.postgresql:postgresql:42.7.3")
-	// mybatisGenerator("org.mybatis.generator:mybatis-generator-core:1.4.0")
+	implementation("org.mybatis.generator:mybatis-generator-core:1.4.2")
 	implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.3")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	developmentOnly("org.springframework.boot:spring-boot-devtools:3.3.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.3.0")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.0")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
 }
 
 kotlin {
