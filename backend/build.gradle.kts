4afa376de9ef32786435c1de7488194d0820a5ab
kobakboa5884 <kobanminmin58@gmail.com>
2024-06-30 01:00:13 +0900

    feat: add open api docs and client build task...

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("org.springframework.boot") version "3.3.1" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
     id("org.openapi.generator") version "7.6.0" apply false
+    id("org.springdoc.openapi-gradle-plugin") version "1.9.0" apply false
 }
 
 group = "com.minmin"
@@ -30,6 +31,8 @@ subprojects {
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.openapi.generator")
+    apply(plugin = "org.springdoc.openapi-gradle-plugin")
 
     // https://kotlinlang.org/docs/gradle-configure-project.html#what-s-next
     dependencies {
@@ -37,6 +40,10 @@ subprojects {
         "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin")
         "implementation"("org.jetbrains.kotlin:kotlin-reflect")
         "developmentOnly"("org.springframework.boot:spring-boot-devtools")
+        "implementation"("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+        "implementation"("org.springdoc:springdoc-openapi-starter-webmvc-api:2.5.0")
+        "implementation"("org.springdoc:springdoc-openapi-ui:1.8.0")
+        "implementation"("com.fasterxml.jackson.module:jackson-module-kotlin:2.17.1")
         "testImplementation"("org.springframework.boot:spring-boot-starter-test")
         "testImplementation"("org.jetbrains.kotlin:kotlin-test-junit5")
         "testRuntimeOnly"("org.junit.platform:junit-platform-launcher")
@@ -52,13 +59,32 @@ subprojects {
     tasks.withType<Test> {
         useJUnitPlatform()
     }
-
     
 }
 
-project(":author:author-api") {
+project(":author-api") {
     dependencies {
         "implementation"(project(":shared"))
+    }
+
+    tasks.withType<org.springdoc.openapi.gradle.plugin.OpenApiGeneratorTask> {
+        apiDocsUrl.set("http://localhost:8080/api-docs")
+        outputDir.set(file("$buildDir/generated/docs"))
+        outputFileName.set("swagger.yml")
+        waitTimeInSeconds.set(60)
+    }
+
+    tasks.register<org.openapitools.generator.gradle.plugin.tasks.GenerateTask>("generateApiClient") {
+        dependsOn("generateOpenApiDocs")
+        generatorName.set("kotlin")
+        inputSpec.set("$buildDir/generated/docs/swagger.yml")
+        outputDir.set("$buildDir/generated/author-api-client")
+        apiPackage.set("com.minmin.author_api_client.api")
+        modelPackage.set("com.minmin.author_api_client.model")
+        invokerPackage.set("com.minmin.author_api_client.invoker")
+        configOptions.set(mapOf(
+            "dateLibrary" to "java17",
+        ))
     }
 }
 
--- backend/settings.gradle.kts
+++ backend/settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "spring-boot-kotlin-api"
-include("author:author-api", "book-api", "comment-api", "shared")
+include("author-api", "book-api", "comment-api", "shared")
