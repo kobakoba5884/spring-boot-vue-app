19d962231d286f7570cdf524fdb66c13ab502528
kobakboa5884 <kobanminmin58@gmail.com>
2024-06-29 20:59:26 +0900

    refactor: upgrade spring boot version by warning.....

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
     kotlin("jvm") version "1.9.24" apply false
-    id("org.springframework.boot") version "3.3.0" apply false
+    id("org.springframework.boot") version "3.3.1" apply false
     id("io.spring.dependency-management") version "1.1.5" apply false
     kotlin("plugin.spring") version "1.9.24" apply false
 }
