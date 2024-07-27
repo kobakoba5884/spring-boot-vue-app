661f5f223d84ac66a2b2761d5c12cd0ee4cc436f
kobakboa5884 <kobanminmin58@gmail.com>
2024-07-27 13:12:45 +0900

    feat: add auth-hands-on-api project.

---
+++ backend/auth-hans-on-api/src/test/kotlin/com/minmin/auth_hans_on_api/AuthHansOnApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.minmin.auth_hans_on_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthHansOnApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
