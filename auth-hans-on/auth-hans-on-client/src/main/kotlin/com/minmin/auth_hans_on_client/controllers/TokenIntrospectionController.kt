3ab9c4976b669d5b4400f439891f46eb4bc4a0ce
kobakboa5884 <kobanminmin58@gmail.com>
2024-07-31 22:38:08 +0900

    feat: add auth endpoint in client controller.

--- auth-hans-on/auth-hans-on-client/src/main/kotlin/com/minmin/auth_hans_on_client/controllers/TokenIntrospectionController.kt
+++ auth-hans-on/auth-hans-on-client/src/main/kotlin/com/minmin/auth_hans_on_client/controllers/TokenIntrospectionController.kt
@@ -1,13 +1,19 @@
 package com.minmin.auth_hans_on_client.controllers
 
 import java.net.URI
+import java.net.URLEncoder
+import java.nio.charset.Charset
+import org.springframework.ui.Model
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.RequestEntity
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.util.UriComponentsBuilder
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import org.springframework.util.LinkedMultiValueMap
 
 @Controller
 class TokenIntrospectionController {
@@ -15,6 +21,21 @@ class TokenIntrospectionController {
     @RequestMapping("/")
     fun index(): String {
         return "index"
+    }
+
+    @PostMapping( "/auth")
+    fun auth() : String{
+        val authorizationUrl = UriComponentsBuilder.fromUriString("http://localhost:8080/auth/realms/auth-hands-on-api/protocol/openid-connect/auth")
+        val params = LinkedMultiValueMap<String, String>()
+        val charset = Charset.defaultCharset().toString()
+        var redirectUrl = ServletUriComponentsBuilder.fromCurrentRequest().replacePath("/gettoken").replaceQuery(null).toUriString()
+        redirectUrl = URLEncoder.encode(redirectUrl, charset)
+        params.add("redirect_uri", redirectUrl)
+        params.add("response_type", "code")
+        params.add("client_id", "auth-hands-on-client")
+        val authUrl = authorizationUrl.queryParams(params).build(true).toUriString()
+
+        return String.format("redirect:%s", authUrl)
     }
 
     @RequestMapping("/call-health")
