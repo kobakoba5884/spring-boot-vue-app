9affc3b797abc4ef66d487e3d89353d4cc507568
Mizuki Kobayashi <84554730+kobakoba5884@users.noreply.github.com>
2024-08-16 10:49:10 +0900

    Revert "Feature/add token introspection"

--- auth-hands-on/auth-hands-on-client/src/main/kotlin/com/minmin/authHandsOnClient/controllers/TokenIntrospectionController.kt
+++ auth-hands-on/auth-hands-on-client/src/main/kotlin/com/minmin/authHandsOnClient/controllers/TokenIntrospectionController.kt
@@ -1,28 +1,30 @@
 package com.minmin.authHandsOnClient.controllers
 
-import com.minmin.authHandsOnClient.configs.ClientAppConfiguration
-import com.minmin.authHandsOnClient.configs.OauthConfiguration
 import com.minmin.authHandsOnClient.dtos.ClientSession
-import com.minmin.authHandsOnClient.services.TokenIntrospectionService
+import com.minmin.authHandsOnClient.dtos.TokenResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
+import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.util.LinkedMultiValueMap
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.client.HttpStatusCodeException
+import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+import org.springframework.web.util.UriComponentsBuilder
 import java.net.URI
+import java.net.URLEncoder
+import java.nio.charset.Charset
 
 @Controller
-class TokenIntrospectionController(
-    val clientSession: ClientSession,
-    val clientConfig: ClientAppConfiguration,
-    val oauthConfig: OauthConfiguration,
-    val tokenIntrospectionService: TokenIntrospectionService,
-) {
+class TokenIntrospectionController(val clientSession: ClientSession) {
     @RequestMapping("/")
     fun index(): String {
         return "index"
@@ -33,7 +35,28 @@ class TokenIntrospectionController(
         @RequestParam(name = "scope", required = false) scope: String?,
     ): String {
         clientSession.scope = scope
-        val authUrl = tokenIntrospectionService.buildAuthorizationUrl(scope)
+
+        val authorizationUrl =
+            UriComponentsBuilder.fromUriString(
+                "http://localhost:8080/realms/auth-hands-on/protocol/openid-connect/auth",
+            )
+        val params = LinkedMultiValueMap<String, String>()
+        val charset = Charset.defaultCharset().toString()
+        var redirectUrl =
+            ServletUriComponentsBuilder.fromCurrentRequest()
+                .replacePath("/gettoken")
+                .replaceQuery(null)
+                .toUriString()
+        redirectUrl = URLEncoder.encode(redirectUrl, charset)
+        params.add("redirect_uri", redirectUrl)
+        params.add("response_type", "code")
+        params.add("client_id", "auth-hands-on-client")
+
+        if (scope != null && !scope.isEmpty()) {
+            params.add("scope", URLEncoder.encode(scope, charset))
+        }
+
+        val authUrl = authorizationUrl.queryParams(params).build(true).toUriString()
 
         return String.format("redirect:%s", authUrl)
     }
@@ -41,27 +64,57 @@ class TokenIntrospectionController(
     @GetMapping("/gettoken")
     fun getToken(
         @RequestParam(name = "code", required = false) code: String,
-        @RequestParam(name = "state", required = false) state: String?,
         @RequestParam(name = "error", required = false) error: String?,
         @RequestParam(name = "error_description", required = false) errorDescription: String?,
         model: Model,
     ): String {
-        if (oauthConfig.formPost) {
-            return "gettoken"
-        }
-
-        error?.let {
+        if (error != null) {
             model.addAttribute("error", error)
             model.addAttribute("errorDescription", errorDescription)
             return "gettoken"
         }
 
-        val token = tokenIntrospectionService.processAuthorizationCodeGrant(code, state)
+        val headers = HttpHeaders()
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED)
+        // TODO
+        headers.setBasicAuth("auth-hands-on-client", "SQRKMhG1pVLmKibdI5JWznD6w9GPXJWo")
+
+        // val charset = Charset.defaultCharset().toString()
+        var redirectUrl =
+            ServletUriComponentsBuilder.fromCurrentRequest()
+                .replacePath("/gettoken")
+                .replaceQuery(null)
+                .toUriString()
+        // redirectUrl = URLEncoder.encode(redirectUrl, charset)
+        val params = LinkedMultiValueMap<String, String>()
+        params.add("code", code)
+        params.add("grant_type", "authorization_code")
+        params.add("redirect_uri", redirectUrl)
+
+        val req =
+            RequestEntity<Any>(
+                params,
+                headers,
+                HttpMethod.POST,
+                URI.create(
+                    "http://keycloak:8080/realms/auth-hands-on/protocol/openid-connect/token",
+                ),
+            )
+        var token: TokenResponse? = null
+
+        try {
+            val res: ResponseEntity<TokenResponse> =
+                RestTemplate().exchange(req, TokenResponse::class.java)
+            token = res.body
+        } catch (e: HttpStatusCodeException) {
+            TokenResponse.withError(e.message, e.getResponseBodyAsString())
+        } catch (e: ResourceAccessException) {
+            TokenResponse.withError(e.message, null)
+        }
 
         if (token?.error != null) {
             model.addAttribute("error", token.error)
             model.addAttribute("errorDescription", token.errorDescription)
-            return "gettoken"
         }
 
         clientSession.setTokensFromTokenResponse(token)
@@ -75,7 +128,7 @@ class TokenIntrospectionController(
             RequestEntity<String>(
                 headers,
                 HttpMethod.GET,
-                URI.create(clientConfig.apiserverUrl + "/health"),
+                URI.create("http://localhost:8888/token-introspection/health"),
             )
 
         val res = RestTemplate().exchange(req, String::class.java)
--- auth-hands-on/auth-hands-on-client/src/main/kotlin/com/minmin/authHandsOnClient/utils/JsonUtil.kt
+++ auth-hands-on/auth-hands-on-client/src/main/kotlin/com/minmin/authHandsOnClient/utils/JsonUtil.kt
@@ -6,41 +6,43 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import java.io.IOException
 
-object JsonUtil {
-    private val mapper: ObjectMapper =
-        ObjectMapper().apply {
-            configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-            configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-            configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
-            setSerializationInclusion(JsonInclude.Include.NON_NULL)
+class JsonUtil {
+    companion object {
+        private val mapper: ObjectMapper =
+            ObjectMapper().apply {
+                configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                setSerializationInclusion(JsonInclude.Include.NON_NULL)
+            }
+
+        fun <T> unmarshal(
+            content: ByteArray,
+            type: Class<T>,
+        ): T {
+            return try {
+                mapper.readValue(content, type)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                throw RuntimeException()
+            }
         }
 
-    fun <T> unmarshal(
-        content: ByteArray,
-        type: Class<T>,
-    ): T {
-        return try {
-            mapper.readValue(content, type)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            throw RuntimeException()
+        fun marshal(obj: Any): String {
+            return marshal(obj, true)
         }
-    }
 
-    fun marshal(obj: Any): String {
-        return marshal(obj, true)
-    }
-
-    fun marshal(
-        obj: Any,
-        indent: Boolean,
-    ): String {
-        return try {
-            mapper.configure(SerializationFeature.INDENT_OUTPUT, indent)
-            mapper.writeValueAsString(obj)
-        } catch (e: IOException) {
-            e.printStackTrace()
-            ""
+        fun marshal(
+            obj: Any,
+            indent: Boolean,
+        ): String {
+            return try {
+                mapper.configure(SerializationFeature.INDENT_OUTPUT, indent)
+                mapper.writeValueAsString(obj)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                ""
+            }
         }
     }
 }
